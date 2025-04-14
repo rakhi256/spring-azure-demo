cedbf10c9c9633c1c0f3279a64322dd891e942b6
rakhi <rrw@kmd.dk>
2025-04-14 15:14:00 +0530

    added new changes

---
+++ src/main/java/com/springazuredemo/spring_azure_demo/SpringAzureDemoApplication.java
@@ -0,0 +1,22 @@
+package com.springazuredemo.spring_azure_demo;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringAzureDemoApplication {
+
+	@GetMapping("/message")
+	public String message(){
+		return "App deployed successfully in Azure";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAzureDemoApplication.class, args);
+	}
+
+}
